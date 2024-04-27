a05501c5ecdd8b4c62e63af32fa45ce122750621
yuWangWu <y.wwu@alumnos.upm.es>
2024-04-27 08:09:48 +0200

    Proyecto base
    Creación del proyecto en VS con el esqueleto. Incluye librería y DLL de freeglut.

---
+++ Chex.cpp
@@ -0,0 +1,85 @@
+/***********************************************
+	Trabajo de Informatica Industrial
+	Curso 2023-2024
+	
+	Proyecto:		Chex
+	Integrantes:	Dario Carnes Blasco
+					Yu Wang Wu
+	Descripcion:	Ajedrez Hexagonal
+***********************************************/
+
+// Definiciones librerias del sistema
+#include <iostream>
+// Definiciones librerias del usuario
+#include "freeglut.h"
+// Definiciones librerias de objetos
+
+
+// Definicion de los Callbacks
+void OnDraw(void);
+void OnKeyboardDown(unsigned char key, int x, int y);
+void OnTimer(int value);
+
+// Codigo
+int main(int argc, char* argv[]) {
+	// Inicializacion y creacion de la ventana.
+	glutInit(&argc, argv);
+	glutInitWindowSize(1920, 1080);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("Chex - Ajedrez Hexagonal");
+
+	// Inicializacion de luces y perspectiva.
+	// Primero se hace la version en 2D y luego 
+	// se pasa a 3D.
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(40.0, 1920 / 1080.0f, 0.1, 150);
+
+	// Registro de Callbacks
+	glutDisplayFunc(OnDraw);
+	glutTimerFunc(0.017, OnTimer, 0); // ~60 FPS
+	glutKeyboardFunc(OnKeyboardDown);
+
+	// Infinite Loop
+	glutMainLoop();
+
+	return 0;
+}
+
+void OnDraw(void) {
+	// Borrar pantalla
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Definir punto de vista
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Codigo del usuario comienza
+
+
+	// Codigo del usuario termina
+
+	glutSwapBuffers();
+}
+
+void OnKeyboardDown(unsigned char key, int x_t, int y_t) {
+	// Codigo del usuario comienza
+
+
+	// Codigo del usuario termina
+
+	glutPostRedisplay();
+}
+
+void OnTimer(int value) {
+	// Codigo del usuario comienza
+
+
+	// Codigo del usuario termina
+
+	glutTimerFunc(0.017, OnTimer, 0);
+	glutPostRedisplay();
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Chex
-Trabajo en grupo de la asignatura de informática industrial curso 2023-24
+Trabajo de Informática Industrial del curso académico 2023-2024.
+El objetivo de este proyecto es desarrollar un programa que permita jugar al ajedrez hexagonal utilizando C++ y OpenGL.
+
+## Integrantes
+
+__Tutor del proyecto__: Pablo San Segundo Carrillo
+
+| Nombre y Apellidos  |          Correo          | Grupo de Teoría |
+| ------------------  | ------------------------ | --------------- |
+| Darío Carnés Blasco | d.cblasco@alumnos.upm.es |      EE309      |
+| Yu Wang Wu          | y.wwu@alumnos.upm.es     |      EE309      |
+
